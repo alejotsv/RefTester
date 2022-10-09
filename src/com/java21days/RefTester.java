c0216d52f01f64559cb2a181a466ff974ccdfa4d
Alejandro Salas <asalas.wd@outlook.com>
2022-10-09 19:53:02 -0400

    Create point objects and reference first one

--- src/com/java21days/RefTester.java
+++ src/com/java21days/RefTester.java
@@ -1,7 +1,17 @@
 package com.java21days;
 
+import java.awt.Point;
+
 public class RefTester {
     public static void main(String[] args) {
-        System.out.println("This is the RefTester main method");
+        Point pt1, pt2;
+        pt1 = new Point(100,100);
+        pt2 = pt1;
+
+        pt2.x = 200;
+        pt2.y = 200;
+
+        System.out.println("Point1:" + "\nX: " + pt1.x + "\nY: " + pt1.y);
+        System.out.println("Point2:" + "\nX: " + pt2.x + "\nY: " + pt2.y);
     }
 }
