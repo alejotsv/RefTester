30eb5f424ba1a5db07f0e69c1f0da198736ec6f0
Alejandro Salas <asalas.wd@outlook.com>
2022-10-09 21:45:31 -0400

    Test reference with Strings

--- src/com/java21days/RefTester.java
+++ src/com/java21days/RefTester.java
@@ -13,5 +13,22 @@ public class RefTester {
 
         System.out.println("Point1:" + "\nX: " + pt1.x + "\nY: " + pt1.y);
         System.out.println("Point2:" + "\nX: " + pt2.x + "\nY: " + pt2.y);
+
+        String str1, str2;
+        str1 = "It's appointment time!";
+        str2 = str1;
+
+        System.out.println("String 1: " + str1);
+        System.out.println("String 2: " + str2);
+        System.out.println("Are these two strings the same object? " + (str1 == str2));
+        System.out.println("Are these two strings equal? " + str1.equals(str2));
+
+        String str3;
+        str3 = "It's appointment time again!";
+        String str4 = new String(str3);
+        System.out.println("String 3: " + str3);
+        System.out.println("String 4: " + str4);
+        System.out.println("Are these two strings the same object? " + (str3 == str4));
+        System.out.println("Are these two strings equal? " + str3.equals(str4));
     }
 }
